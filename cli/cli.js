a00f601fef583361b01ee4c299be05935a75ed39
csoltero109 <chrissoltero109@gmail.com>
2018-03-24 16:20:11 -0700

    Revert "platform.js added"
    This reverts commit bf64e0b779e1f93aa1ffbfedcd1381d86aa84060.

--- cli/cli.js
+++ cli/cli.js
@@ -4,7 +4,6 @@ const
 
 const flags = yargs.usage('$0: Usage <cmd> [options]')
     .command(require('./searchGame'))
-	.command(require('./searchPlatforms'))
     .demandCommand(1, "Please input a command")
     .help('help')
     .argv
