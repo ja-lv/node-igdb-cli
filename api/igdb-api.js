feb1b1d382272fcf70fa2e7c56a681e16cb24ef8
Jeffry Alvarado <alvaradojeffry95@gmail.com>
2018-03-23 23:06:26 -0700

    now game search uses the api directly with node request

--- api/igdb-api.js
+++ api/igdb-api.js
@@ -1,7 +1,8 @@
 const  
     config = require('./config'),
     igdb = require('igdb-api-node').default,
-    client = igdb(config.key)
+    client = igdb(config.key),
+    request = require('request')
 
 //takes in args, ex:
 // {
@@ -9,7 +10,10 @@ const
 //     search: 'metal gear'
 // }
 //refer to https://igdb.github.io/api/references/ for args
-exports.getGame = (args) =>{
+
+
+//get game using node wrapper:
+exports.getGameWrapper = (args) =>{
     return new Promise(function(fulfill, reject){
         client.games(args).then(response => {
             fulfill(response.body)
@@ -18,3 +22,30 @@ exports.getGame = (args) =>{
         });  
     })
 }
+
+exports.getGame = (args) =>{
+    return new Promise(function(fulfill, reject){
+        request(
+        {
+            url: config.url + config.games + argsUrl(args),
+            headers: {
+                'user-key': config.key,
+                'accept': "application/json"
+            }
+        }, 
+        (error, res, body) =>{
+            if(error) reject(error)
+
+            fulfill(JSON.parse(body))
+        })
+    })
+}
+
+function argsUrl({fields = '', ids = '', search = '', limit = ''}){
+    const url = "/" 
+                + ((Array.isArray(ids) && ids[0]) ? ids.join(',') + "?" : (search && typeof search == "string") ? "?search=" + search : '?')
+                + ((Array.isArray(fields) && fields.length) ? "&fields=" + fields.join(',') : (fields == "*") ? "&fields=*" : '')
+                + ((typeof limit == "number" && limit > 0) ? "&limit=" + limit : '')
+
+    return url
+}   
